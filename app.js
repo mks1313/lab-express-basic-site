39a5250664acd6c33d1012de1d73491715cfe281
mks1313 <139466229+mks1313@users.noreply.github.com>
2023-11-12 15:38:30 +0100

    Solved Lab

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.static('public'));
+
+app.get('/', (request, response, next) => response.sendFile(__dirname + '/views/home.html'));
+
+app.get('/about', (request, response, next) => response.sendFile(__dirname + '/views/about.html'));
+
+app.get('/works', (request, response, next) => response.sendFile(__dirname + '/views/works.html'));
+
+app.get('/gallery', (request, response, next) => response.sendFile(__dirname + '/views/gallery.html'));
+ 
+// Server Started
+app.listen(3000, () => {
+console.log('My first app listening on port 3000!'
+)});
+
+
+
+
+
